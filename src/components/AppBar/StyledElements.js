c4f975816f2bf345d6b81d387d41eb572ab52450
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2021-10-15 22:36:03 +0200

    completing header styles

--- src/components/AppBar/StyledElements.js
+++ src/components/AppBar/StyledElements.js
@@ -1,15 +1,144 @@
 import styled, { css } from "styled-components";
 
-const Header = styled.header`
-  height: 70px;
-  max-height: 70px;
+const Header = styled.header``;
+
+const HeaderWrapper = styled.div`
   width: 100%;
-  background-color: var(--clr-white);
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
     rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
   position: fixed;
   z-index: 10;
-  opacity: 0.95;
+
+  ::after {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to bottom, var(--clr-dark-blue), transparent);
+    z-index: -1;
+    display: ${(props) => (props.isOpen ? "block" : "none")} !important;
+
+    @media (min-width: 768px) {
+      display: none !important;
+    }
+  }
 `;
 
-export { Header };
+const NavWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  max-height: 70px;
+
+  ${css`
+    button {
+      display: none;
+    }
+    @media (min-width: 768px) {
+      button {
+        display: block;
+      }
+    }
+  `}
+`;
+
+const NavLogoWrapper = styled.div``;
+
+const NavLogo = styled.img``;
+
+const NavToggle = styled.button`
+  border: 0;
+  outline: none;
+  background-color: transparent;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  width: 40px;
+  cursor: pointer;
+  padding-block: 10px;
+  ${css`
+    @media (min-width: 768px) {
+      display: none !important;
+    }
+    span {
+      transform-origin: center;
+      transition: transform var(--delay);
+    }
+    span:nth-child(2) {
+      opacity: ${(props) => (props.isOpen ? "0" : "1")};
+    }
+    span:nth-child(1) {
+      transform: ${(props) =>
+        props.isOpen ? "translateY(7px) rotateZ(45deg)" : "none"};
+    }
+    span:nth-child(3) {
+      transform: ${(props) =>
+        props.isOpen ? "translateY(-7px) rotateZ(-45deg)" : "none"};
+    }
+    }
+  `};
+`;
+
+const NavToggleLine = styled.span`
+  display: block;
+  width: 100%;
+  height: 2px;
+  background-color: var(--clr-dark-blue);
+`;
+
+const NavList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  flex-direction: column;
+  position: absolute;
+  top: calc(70px + 2rem);
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: var(--clr-white);
+  border-radius: var(--rad);
+  width: calc(100% - 4rem);
+  max-width: 576px;
+  padding-block: 2rem;
+  z-index: 1;
+
+  @media (min-width: 768px) {
+    display: flex !important;
+    flex-direction: row;
+    position: static;
+    transform: none;
+    width: unset;
+    padding: 0;
+    background-color: transparent;
+  }
+`;
+
+const NavItem = styled.li``;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  color: var(--clr-dark-blue);
+  font-weight: 400;
+`;
+
+export {
+  Header,
+  HeaderWrapper,
+  NavWrapper,
+  NavLogoWrapper,
+  NavLogo,
+  NavToggle,
+  NavToggleLine,
+  NavList,
+  NavItem,
+  NavLink,
+};
--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,6 +1,68 @@
-import React from "react";
-import { Header } from "./StyledElements";
+import React, { useState } from "react";
+import { Button, Container } from "../Common/StyledElements";
+import {
+  Header,
+  HeaderWrapper,
+  NavItem,
+  NavLink,
+  NavList,
+  NavLogo,
+  NavLogoWrapper,
+  NavToggle,
+  NavToggleLine,
+  NavWrapper,
+  NavOverlay,
+} from "./StyledElements";
+import Logo from "../../assets/images/logo.svg";
 
 export default function Error() {
-  return <Header></Header>;
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <Header>
+      <HeaderWrapper isOpen={isOpen}>
+        <Container style={{ backgroundColor: "var(--clr-white)" }}>
+          <NavWrapper>
+            <NavLogoWrapper>
+              <NavLink href="/">
+                <NavLogo src={Logo} alt={"logo"} />
+              </NavLink>
+            </NavLogoWrapper>
+            {/* {isOpen && <NavOverlay />} */}
+            <NavList isOpen={isOpen}>
+              <NavItem>
+                <NavLink href={"#"}>{"Home"}</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={"#"}>{"About"}</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={"#"}>{"Contact"}</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={"#"}>{"Blog"}</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={"#"}>{"Careers"}</NavLink>
+              </NavItem>
+            </NavList>
+
+            <Button gradient rounded>
+              {"Request Invite"}
+            </Button>
+
+            <NavToggle isOpen={isOpen} onClick={handleToggle}>
+              <NavToggleLine></NavToggleLine>
+              <NavToggleLine></NavToggleLine>
+              <NavToggleLine></NavToggleLine>
+            </NavToggle>
+          </NavWrapper>
+        </Container>
+      </HeaderWrapper>
+    </Header>
+  );
 }
